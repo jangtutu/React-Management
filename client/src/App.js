289359f376df1c484313216c504498e3f2cc7920
장원 <jangwon_s@naver.com>
2024-08-27 18:14:07 +0900

    Add Node.js Express Server modules

--- client/src/App.js
+++ client/src/App.js
@@ -21,26 +21,26 @@ const StyledTable = styled(Table)({
 
 const customers = [
   {
-  'id' : '1',
-  'name' : '장원',
-  'birthday' : '971008',
-  'gender' : '남자',
-  'job' : '대학생'
-},  
-{
-  'id' : '2',
-  'name' : '장종대',
-  'birthday' : '570402',
-  'gender' : '남자',
-  'job' : '백수'
-},  
-{
-  'id' : '3',
-  'name' : '최미경',
-  'birthday' : '640103',
-  'gender' : '여자',
-  'job' : '주부'
-}
+    'id': '1',
+    'name': '장원',
+    'birthday': '971008',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': '2',
+    'name': '장종대',
+    'birthday': '570402',
+    'gender': '남자',
+    'job': '백수'
+  },
+  {
+    'id': '3',
+    'name': '최미경',
+    'birthday': '640103',
+    'gender': '여자',
+    'job': '주부'
+  }
 ]
 
 class App extends Component {
@@ -51,26 +51,26 @@ class App extends Component {
         <StyledTable>
           <TableHead>
             <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
+              <TableCell>번호</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-        {
-          customers.map(c => {
-            return <Customer
-              key = {c.id}
-              id = {c.id}
-              name = {c.name}
-              birthday = {c.birthday}
-              gender = {c.gender}
-              job = {c.job}
-            />
-          })
-        }
+            {
+              customers.map(c => {
+                return <Customer
+                  key={c.id}
+                  id={c.id}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              })
+            }
           </TableBody>
         </StyledTable>
       </StyledPaper>
