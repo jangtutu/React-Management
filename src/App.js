7aa2d0caca4b50b3632100c2a434d5fb13b894bc
장원 <jangwon_s@naver.com>
2024-08-27 14:25:40 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './component/Costomer';
+
+const customers = [
+  {
+  'id' : '1',
+  'image' : 'https://placeimg.com/640/480/any',
+  'name' : '장원',
+  'birthday' : '971008',
+  'gender' : '남자',
+  'job' : '대학생'
+},  
+{
+  'id' : '2',
+  'image' : 'https://placeimg.com/640/480/any',
+  'name' : '장종대',
+  'birthday' : '570402',
+  'gender' : '남자',
+  'job' : '백수'
+},  
+{
+  'id' : '3',
+  'image' : 'https://placeimg.com/640/480/any',
+  'name' : '최미경',
+  'birthday' : '640103',
+  'gender' : '여자',
+  'job' : '주부'
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>Management System</h2>
+      <div>
+        {
+          customers.map(c => {
+            return <Customer
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />
+          })
+        }
       </div>
     );
   }
