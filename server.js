6a88dc9710f48ba20f8c898654be19514131c3a7
장원 <jangwon_s@naver.com>
2024-08-29 19:08:08 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,30 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) =>  {
-    res.send([
-        {
-          'id': '1',
-          'name': '장원',
-          'birthday': '971008',
-          'gender': '남자',
-          'job': '대학생'
-        },
-        {
-          'id': '2',
-          'name': '장종대',
-          'birthday': '570402',
-          'gender': '남자',
-          'job': '백수'
-        },
-        {
-          'id': '3',
-          'name': '최미경',
-          'birthday': '640103',
-          'gender': '여자',
-          'job': '주부'
-        }
-      ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      } 
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
